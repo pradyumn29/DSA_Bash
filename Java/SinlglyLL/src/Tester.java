1ef8ed65c2cd700e8d73a4a333b8c856c64dfa89
Pradyumn Patil <119255271+pradyumn29@users.noreply.github.com>
2023-07-04 01:13:01 +0530

    Tester.java

--- Java/SinlglyLL/src/Tester.java
+++ Java/SinlglyLL/src/Tester.java
@@ -7,6 +7,36 @@ public class Tester {
 		SinglyLL list=new SinglyLL();
 		
 		list.insertAtFirst(11);
+		list.insertAtFirst(21);
+		list.insertAtFirst(51);
+		list.insertAtFirst(61);
+		
+		list.Display();
+		System.out.println("no of nodes:"+list.Count());
+		
+		list.insertAtLast(110);
+		list.insertAtLast(105);
+		list.insertAtLast(121);
+		list.Display();
+		System.out.println("no of nodes:"+list.Count());
+		
+		list.insertAtPos(1000, 3);
+		list.Display();
+		System.out.println("no of nodes:"+list.Count());
+		
+		list.deleteAtFirst();
+		list.Display();
+		System.out.println("no of nodes:"+list.Count());
+		
+		list.deleteAtLast();
+		list.Display();
+		System.out.println("no of nodes:"+list.Count());
+		
+		list.DeleteAtpos(3);
+		list.Display();
+		System.out.println("no of nodes:"+list.Count());
+		
+		
 	}
 
 }
